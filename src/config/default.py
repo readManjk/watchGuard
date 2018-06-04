340f8a2fa19c7aa03c79a1061ea28fc5f6fc54d5
readManjk <506357124@qq.com>
2018-06-04 19:25:27 +0800

    完成product产品类和watchguard获取产品，control层调度

--- src/config/default.py
+++ src/config/default.py
@@ -5,3 +5,7 @@
 @time:2018/06/03 12:40:11
 """
 LOG_FILE_NAME = "C:\\watchGuard.log"
+COMPARE_URL = "https://www.watchguard.com/wgrd-products/appliances-compare"
+USER_PRODUCT = ['WatchGuard® Firebox M Series', 'WatchGuard Firebox T Series']
+PERFORMANCE = ['Firewall Throughput', 'VPN Throughput', 'AV Throughput', 'IPS Throughput',
+               'UTM Throughput', 'Concurrent Sessions*']
